a5a580af5619cf8bec74fc87f6c3a56c22819ea0
rysmld <78962313+rysmld@users.noreply.github.com>
2022-03-17 10:29:33 +0800

    Update main.cpp

--- AsyncActivity3172022/main.cpp
+++ AsyncActivity3172022/main.cpp
@@ -1,75 +1,156 @@
-#define GLEW_STATIC // <----- define GLEW_STATIC
-#include<iostream>
-#include <GL/glew.h> // <---- additional header files
-#include <GL/glut.h>
-using namespace std;
-void display();
-void triangle();
-void init();
-void keyBoardMonitor(unsigned char key, int x, int y);
-GLuint VBOid;
-bool initTriangle = true;
-float angle = 1.0f;
+#define GLEW_STATIC
+#define BUFFER_OFFSET(i) ((void*)(i))
 
-GLfloat trianglevertices[] = {
-	0.0f, .75f, 0.0f,
-	-0.75f, 0.0f, 0.0f,
-	0.75f, 0.0f, 0.0f
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <stdio.h>
+
+using namespace std;
+
+void displayObject();
+void displayTriangle();
+void animateTriangle(int value);
+void init();
+
+void GLAPIENTRY
+MessageCallback( GLenum source,
+	GLenum type,
+	GLuint id,
+	GLenum severity,
+	GLsizei length,
+	const GLchar* message,
+	const void* userParam );
+	GLuint VBOid;
+	void keyMonitor(unsigned char key, int x, int y);
+	bool initTriangle = true;
+
+	float angle=1.0f;
+
+	GLfloat vertices[] ={
+	//------------------- Triangle
+	0.0f, 1.0f, 0.0f, 
+	-1.0f, -1.0f, 1.0f,
+	1.0f, -1.0f, 1.0f,
+	
+	0.0f, 1.0f, 0.0f,
+	1.0f,-1.0f, 1.0f,
+	1.0f, -1.0f,-1.0f,
+	
+	0.0f, 1.0f, 0.0f,
+	1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	
+	0.0f, 1.0f, 0.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f, 1.0f,
+};
+
+GLfloat colorstriangle[] = {
+	1.0f, 0.0f, 0.0f,
+	0.0f, 1.0f, 0.0f,
+	0.0f, 0.0f, 1.0f,
+	
+	1.0f, 0.0f, 0.0f,
+	0.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f,
+	
+	1.0f, 0.0f, 0.0f,
+	0.0f, 1.0f, 0.0f,
+	0.0f, 0.0f, 1.0f,
+	
+	1.0f,0.0f,0.0f,
+	0.0f,0.0f,1.0f,
+	0.0f,1.0f,0.0f
 };
 
 int main(int argc, char** argv){
 	glutInit(&argc, argv);
-	glutInitWindowSize(800,600);
-	glutInitWindowPosition(100,100);
 	glutCreateWindow("simple");
-	glutKeyboardFunc(keyBoardMonitor);
-	glutDisplayFunc(display);
-	GLenum err = glewInit(); 
-		if (err == GLEW_OK) {
-			glutMainLoop(); 
-	} 
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH); 
+
+	init();
+
+	glutDisplayFunc(displayObject);
+	glutKeyboardFunc(keyMonitor);
+	glutInitWindowSize(800,600);
+	GLenum err = glewInit();
+		if(err == GLEW_OK){
+			glEnable ( GL_DEBUG_OUTPUT );
+			glDebugMessageCallback( MessageCallback, 0 );
+			printf("OpenGL version supported by this platform (%s): \n",
+			glGetString(GL_VERSION));
+			printf("OpenGL vendor (%s): \n", glGetString(GL_VENDOR));
+			glutMainLoop();
+		}
 }
 
-void triangle(){
-	if(initTriangle){
-		glGenBuffers(1, &VBOid);
+void displayTriangle(){
+if(initTriangle){
+	glGenBuffers(1, &VBOid);
 	glBindBuffer(GL_ARRAY_BUFFER, VBOid);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*9, trianglevertices, GL_STATIC_DRAW);
+	
+	glBufferData(GL_ARRAY_BUFFER, ((9*4) + (9*4)) * sizeof(GLfloat), vertices, GL_DYNAMIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, (9*4) * sizeof(GLfloat),(9*4) * sizeof(GLfloat), colorstriangle);
+	
 	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_COLOR_ARRAY);
+	
+	glVertexPointer(3, GL_FLOAT, 0, 0);
+	glColorPointer(3, GL_FLOAT,0,BUFFER_OFFSET((9*4)*sizeof(GLfloat) ));
+	
+	initTriangle = false;
+}
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	glTranslatef(0.0f, -0.5f, 0.0f);
-	glScalef(.75f,.75f,0.0f);
-	initTriangle= false;
-}
-
 	glPushMatrix();
-	glRotatef(angle,0.0f,0.0f,1.0);
-	glVertexPointer(3, GL_FLOAT, 0, 0);
-	glDrawArrays(GL_TRIANGLES,0,6);
+	glRotatef(angle,0.0f,1.0f,0.0f);
+	
+	glBegin(GL_TRIANGLES); //Displaying Six Sides
+	for(int i=0; i < 12; i++) {
+		glArrayElement(i);
+	}
+	
+	glEnd();
+	glFlush();
 	glPopMatrix();
 }
 
 void animateTriangle(int value){
-	angle++;
-	glutPostRedisplay();
-	
-}
-
-void display(){
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor4f(.16f,.72f,.08f,1.0f);
-	triangle();
-	glFlush();
-	glutTimerFunc(200,animateTriangle,1);
-}
-
-void keyBoardMonitor(unsigned char key, int x, int y){
-	if(key == 'x'){
+	if(angle>360)
+		angle=0;
+		angle++;
+		glutPostRedisplay();
+	}
+	void keyMonitor(unsigned char key, int x, int y){
+	if(key=='x'){
 		glDisableClientState(GL_VERTEX_ARRAY);
+		glDisableClientState(GL_COLOR_ARRAY);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 		glDeleteBuffers(1, &VBOid);
 		exit(0);
-	} 
+	}
+}
 
+void displayObject(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	displayTriangle();
+	glutTimerFunc(50,animateTriangle,1);
+	glFlush();
+}
+
+void GLAPIENTRY MessageCallback( GLenum source,
+	GLenum type,
+	GLuint id,
+	GLenum severity,
+	GLsizei length,
+	const GLchar* message,
+	const void* userParam )
+{
+	fprintf( stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR**" : "" ), type, severity, message );
+}
+
+void init(){
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-4.0,4.0,-4.0,4.0, -4.0,4.0);
 }
